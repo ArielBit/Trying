0d36d813d704d2dfa65c5a265d7fede176534156
Ariel Toure <arielz360007@gmail.com>
2025-12-16 16:26:07 +0000

    Add initial View.php file with basic structure

---
+++ View.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="uft-8">
+    <meta name="" content="">
+  </head>
+  <body>
+    <?php
+   if($data as $row) {
+  
+    }
+  ?>
+  </body>
+</html>
