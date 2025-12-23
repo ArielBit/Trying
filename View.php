4a05ce8c59a930e83e7c828d9c23727371868820
Ariel Toure <arielz360007@gmail.com>
2025-12-23 12:28:18 +0000

    Update View.php

--- View.php
+++ View.php
@@ -9,10 +9,30 @@ session_start();
   </head>
   <body>
 
-    <?php 
+    <?php
+//Coordonnée du serveur sql
+$dbname="db_try";
+$host='127.0.0.9';
+$username="root";
+$password="Ariel&007";
+$port=3309;
 
-
+//Connexion au serveur sql
+try{
+$connect= new PDO('mysql:host=$host; dbname=$dbname; port=$port; charset=utf8mb4', $username, $password);
+$connect-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+}catch(PDOException $e) {
+  echo "Erreur lors de l'insertion des données". $e->getMessage();
+  exit();
+}
+//Récupéreration des données.
+  $stmt=$connect->prepare('SELECT id, user, email FROM tri');
+  $stmt->execute();
+//Recuperation de toute les lignes. 
+$data=$stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
+
     
     <?php foreach ($data as $row): ?>
 <p>
@@ -25,7 +45,7 @@ session_start();
   Email: <?php echo htmlspecialchars($row["email"]); ?>
 </p>
 
-  <p>
+  <?php endforeach; ?>
   
 
     
