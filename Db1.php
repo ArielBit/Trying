cfb69e3fe238217f5777e27835881ae09adc0bfe
Ariel Toure <arielz360007@gmail.com>
2025-12-23 10:00:53 +0000

    Refactor database connection variables and error handling

--- Db1.php
+++ Db1.php
@@ -1,17 +1,17 @@
 <?php
 //Coordonnée du serveur sql
-$Dbname="db_try";
-$Host='127.0.0.9';
-$Username="root";
-$Password="Ariel&007";
-$Port=3309;
+$dbname="db_try";
+$host='127.0.0.9';
+$username="root";
+$password="Ariel&007";
+$port=3309;
 
 //Connexion au serveur sql
 try{
-$Connect= new PDO('mysql:host=$Host, dbname=$Dbname, port=$Port', $Username, $Password);
-$Connect-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$connect= new PDO('mysql:host=$host; dbname=$dbname; port=$port; charset=utf8mb4', $username, $password);
+$connect-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   
-}catch(PDO EXCEPTION $e) {
+}catch(PDOException $e) {
   echo "Erreur lors de l'insertion des données". $e->getMessage();
 }
 ?>
